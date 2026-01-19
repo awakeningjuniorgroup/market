0df2184880486d161e8d770ab2fd143f606f6521
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-01-19 15:00:15 +0100

    Update CartDrawer.jsx

--- frontend/src/components/layout/CartDrawer.jsx
+++ frontend/src/components/layout/CartDrawer.jsx
@@ -4,18 +4,18 @@ import CartContents from "../cart/CartContents";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 const CartDrawer = ({ drawerOpen, toggleCartDrawer}) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const {user, guestId} = useSelector((state) => state.auth);
   const {cart} = useSelector((state) => state.cart);
   const userId = user ? user._id : null;
 
   const handleCheckout = () => {
     toggleCartDrawer();
-    if (!user) {
-      navigate("/login?redirect=checkout")
-    }else{
-      navigate("/checkout")
-    }
+    // if (!user) {
+    //   navigate("/login?redirect=checkout")
+    // }else{
+    //   navigate("/checkout")
+    // }
   }
    
   return (
