444c5237a636e654b6f6a696e1251cd9d30aa343
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-01-26 16:06:14 +0100

    Update Checkout.js

--- backend/models/Checkout.js
+++ backend/models/Checkout.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const checkoutItemSchema = new mongoose.Schema(
   {
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
     name: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
