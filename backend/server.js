008bdec94795d8edea42347f02bf8be7b6bc3d77
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-01-29 12:06:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
-  origin: "*",
+  origin: "https://kams-market12.onrender.com",
   credentials: true,
   allowedHeaders: ["Authorization", "Content-Type"],
 }
