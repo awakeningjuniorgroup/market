f8408575494a641776d99a5772ec90f1d366a8fc
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-02-07 14:55:28 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,6 +58,7 @@ const App = () => {
             <Route path="orders" element={<OrderManagement />} />
           </Route>
         </Routes>
+         <WhatsAppButton />
       </BrowserRouter>
     </Provider>
   );
