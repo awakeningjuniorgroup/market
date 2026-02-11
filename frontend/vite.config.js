fbeccba3195d5bee64aa4a9a7ee618d0aad89a90
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-02-11 23:32:13 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // ✅ permet à React Router de gérer les routes sans reload
+    historyApiFallback: true
+  },
+  build: {
+    // optionnel : utile pour Render/Netlify/Vercel
+    outDir: 'dist'
+  }
 })
