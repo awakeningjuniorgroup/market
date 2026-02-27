2cb9317e420dc0ff554e3f0454326a397b352639
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-02-27 16:29:10 +0100

    Update OrderManagement.jsx

--- frontend/src/components/admin/OrderManagement.jsx
+++ frontend/src/components/admin/OrderManagement.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { fetchAllOrders, updateOrderStatus } from '../../../slice/adminOrderSlice';
+import { fetchAllOrders, updateOrderStatus } from '../../slice/adminOrderSlice';
 
 const OrderManagement = () => {
   const dispatch = useDispatch();
