17ca7fd0c4f87a0e1da7981c4f6945e556b13ebd
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-03-04 19:13:44 +0100

    Update MyOrdersPage.jsx

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -23,7 +23,7 @@ const MyOrdersPage = () => {
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-6">
       <h2 className="text-xl sm:text-2xl font-bold mb-6">My Checkouts</h2>
-      <div className="relative shadow-md sm:rounded-lg overflow-hidden">
+      <div className="relative shadow-md sm:rounded-lg overflow-x-auto">
         <table className="min-w-full text-left text-gray-500">
           <thead className="bg-gray-100">
             <tr>
