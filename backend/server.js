58cbbec7e3c0c7219a9b4eddfdf7fcf87cbd425f
awakeningjuniorgroup <awakeningjuniorgroup@gmail.com>
2026-03-05 14:02:42 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.use("/api/admin/orders", adminOrderRoutes);
 
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on  http://localhost:${PORT}`);
 });
 
 // Global error handler
